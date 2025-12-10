b584a2de6c741168d90c59beb9bb21803b58f72d
DarrenDeo <darrenwijaya46@gmail.com>
2025-12-10 11:48:10 +0700

    test ci

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -22,4 +22,11 @@ const eslintConfig = [
   },
 ];
 
+rules: {
+  "@typescript-eslint/no-explicit-any": "off",
+  "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+  "react/no-unescaped-entities": "off",
+}
+
+
 export default eslintConfig;
